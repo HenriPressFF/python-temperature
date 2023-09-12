3fa134a1f8ce42296c7260dc231d35d08a84cb46
Henrique <henrique.faraco@hotmail.com>
2023-09-12 11:28:57 +0200

    Python module for raspberry

--- temp.py
+++ temp.py
@@ -2,6 +2,18 @@
 
 TEMP_PATH="/sys/class/thermal/thermal_zone0/"
 
-fo = open(TEMP_PATH + "temp", "r")
-temp =fo.read()
-print(temp)
+#fo = open(TEMP_PATH + "temp", "r")
+#temp =fo.read()
+#print(temp)
+
+def readTemp():
+    fo = open(TEMP_PATH+ "temp" , "r")
+    temp = fo.read()
+    return int(temp) *0.001
+
+def averageTemp(n):
+    allvalues = 0
+    for x in range(n):
+            allvalues = readTemp() + allvalues
+    return allvalues/n
+
